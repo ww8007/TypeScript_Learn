079bd325d928b7034b2d9ca2d3e2a9d7f01ff85e
ww8007 <wshmin@naver.com>
2021-03-03 19:58:36 +0900

    Setting Files

---
+++ Ts_Practice/dist/practice.js
@@ -0,0 +1,17 @@
+"use strict";
+let count = 0;
+count += 1;
+// 문자
+const message = "hello world";
+// 불리언
+const done = false;
+// 배열
+const numbers = [1, 2, 3];
+// 문자열
+const messages = ["heelo", "world"];
+// 문자열 일 수도 있고 undefined 일 수도 있을 경우
+let mightbeUndefined = undefined;
+//숫자 이거나 null 일수도 있는 경우
+let numllableNumber = 5;
+let color = "red";
+color = "yello";
---
+++ Ts_Practice/src/practice.ts
@@ -0,0 +1,6 @@
+function returnNothing(): string | number {
+  console.log("asdlfkjasl");
+  return 1;
+}
+
+returnNothing();
